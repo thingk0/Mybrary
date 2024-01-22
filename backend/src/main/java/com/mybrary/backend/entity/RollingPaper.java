6043a61fc66ae501e3a0aa5f49ea2ed5b3835086
박헤선 <qkr8364@naver.com>
2024-01-22 21:03:09 +0900

    FEAT: RollingPaper 엔티티 생성

---
+++ backend/src/main/java/com/mybrary/backend/entity/RollingPaper.java
@@ -0,0 +1,36 @@
+package com.mybrary.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RollingPaper extends BaseEntity{
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "rollingpaper_id")
+  private Long id;
+
+//  @OneToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "mybrary_id")
+//  private Mybrary mybrary;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "imgae_id")
+  private Image rollingpaperImage;
+
+}
