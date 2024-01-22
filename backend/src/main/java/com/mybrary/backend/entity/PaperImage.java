5cd5d40ed0efc4510a85aa980427a63a4f5302d3
박헤선 <qkr8364@naver.com>
2024-01-22 21:00:10 +0900

    FEAT: PaperImage 엔티티 생성

---
+++ backend/src/main/java/com/mybrary/backend/entity/PaperImage.java
@@ -0,0 +1,39 @@
+package com.mybrary.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaperImage {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "paper_image_id")
+  private Long id;
+
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "papre_id")
+//  private Paper paper;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "image_id")
+  private Image image;
+
+  private int imageSeq;
+
+
+}
