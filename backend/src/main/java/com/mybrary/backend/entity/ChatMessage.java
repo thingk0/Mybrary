d696fa99b2f72376c9bc20b896970daad696f617
박헤선 <qkr8364@naver.com>
2024-01-22 20:16:06 +0900

    FEAT: ChatMessage 엔티티 생성

---
+++ backend/src/main/java/com/mybrary/backend/entity/ChatMessage.java
@@ -0,0 +1,41 @@
+package com.mybrary.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage extends BaseEntity{
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "chat_message_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "chat_room_id")
+  private ChatRoom chatRoom;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member receiver;
+
+  private String message;
+  private Long threadId;
+  private Long paperId;
+  private boolean isRead;
+
+}
