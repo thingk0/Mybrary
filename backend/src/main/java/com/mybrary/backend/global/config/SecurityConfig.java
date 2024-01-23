c33e0f66e82c198a99135e685c004aa09c01a4cf
고명성 <audtjd9080@naver.com>
2024-01-24 00:13:22 +0900

    CHORE: Security 설정 수정 및 Swagger 경로 보안 예외 처리 추가
    - Spring Security 설정에서 `HttpSecurity`의 `authorizeHttpRequests` 섹션 수정.
    - Swagger 관련 경로에 대한 보안 예외 처리 추가하여 API 문서 접근 용이성 향상.

--- backend/src/main/java/com/mybrary/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/mybrary/backend/global/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,27 +34,20 @@ public class SecurityConfig {
 
         security
             .authorizeHttpRequests((authorize -> {
-//                authorize.requestMatchers("/**");
+                authorize.requestMatchers(
+                    "/v2/api-docs",
+                    "/v3/api-docs",
+                    "/swagger-ui/**",
+                    "/webjars/**",
+                    "/swagger/**",
+                    "/api-docs/**",
+                    "/swagger-ui/**",
+                    "/swagger-resources/**"
+                ).permitAll();
                 authorize.anyRequest().permitAll();
             }));
 
         return security.build();
     }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                           .requestMatchers(
-                               "/v2/api-docs",
-                               "/v3/api-docs",
-                               "/swagger-ui/**",
-                               "/webjars/**",
-                               "/swagger/**",
-                               "/api-docs/**",
-                               "/swagger-ui/**",
-                               "/swagger-resources/**"
-                           );
-    }
-
 
 }
