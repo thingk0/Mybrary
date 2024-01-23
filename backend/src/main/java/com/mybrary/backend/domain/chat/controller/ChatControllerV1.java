ad1a12d0d1270fa1611b17670cc39292641605f1
박헤선 <qkr8364@naver.com>
2024-01-23 16:59:38 +0900

    FEAT: ChatController 추가

---
+++ backend/src/main/java/com/mybrary/backend/domain/chat/controller/ChatControllerV1.java
@@ -0,0 +1,50 @@
+package com.mybrary.backend.domain.chat.controller;
+
+import com.mybrary.backend.domain.chat.chat_message.entity.ChatMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/chat")
+public class ChatControllerV1 {
+
+    @Operation(summary = "채팅방 리스트 조회", description = "자신의 채팅방 리스트 조회")
+    @GetMapping
+    public ResponseEntity<?> getAllChatRoomList() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "채팅방 나가기", description = "채팅방 나가기 (채팅참여 삭제처리)")
+    @DeleteMapping
+    public ResponseEntity<?> deleteChatRoom() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "채팅방의 메세지 리스트 조회", description = "채팅방의 메세지 리스트 조회")
+    @GetMapping("/{chatRoomId}/message")
+    public ResponseEntity<?> getAllChatList(@PathVariable Long chatRoomId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "채팅방이 없는 사람과의 메세지 리스트 조회", description = "한번도 안했으면 리스트가 비어있고, 채팅방을 나간거면 리스트가 있음")
+    @GetMapping("/message")
+    public ResponseEntity<?> enterChatRoom(@RequestParam Long memberId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "채팅 메세지 보내기", description = "채팅 메세지 보내기")
+    @PostMapping("/{chatRoomId}/message")
+    public ResponseEntity<?> createChat(@PathVariable Long chatRoomId, @RequestBody ChatMessage message) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
