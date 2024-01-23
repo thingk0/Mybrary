1bacc5931bf601908bdd1c504e76d51a4522d947
고명성 <audtjd9080@naver.com>
2024-01-23 16:51:27 +0900

    REFACTOR: NotificationController의 API 경로 및 버전 업데이트
    - NotificationController 클래스의 API 경로를 `/api/v1/notification`로 변경
    - 클래스 이름을 `NotificationControllerV1`로 업데이트
    - 각 API 엔드포인트의 경로를 새로운 버전 경로에 맞게 수정

--- backend/src/main/java/com/mybrary/backend/domain/notification/controller/NotificationControllerV1.java
+++ backend/src/main/java/com/mybrary/backend/domain/notification/controller/NotificationControllerV1.java
@@ -1,26 +1,25 @@
 package com.mybrary.backend.domain.notification.controller;
 
-import com.mybrary.backend.domain.member.entity.Member;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NotificationController {
+@RequestMapping("/api/v1/notification")
+public class NotificationControllerV1 {
 
     @Operation(summary = "나의 알림 조회", description = "알림 조회")
-    @GetMapping("/notification")
+    @GetMapping
     public ResponseEntity<?> getAllNotification() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Operation(summary = "알림 삭제", description = "알림 삭제")
-    @DeleteMapping("/notification")
+    @DeleteMapping
     public ResponseEntity<?> deleteNotification() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
