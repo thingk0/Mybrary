9b56d77d8efd28655e8d6a9fcacc1e56b333969f
tiger3127 <tiger6453127@naver.com>
2024-01-24 01:08:44 +0900

    FEAT: Controller 생성 및 컨벤션에 맞게 수정

---
+++ backend/src/main/java/com/mybrary/backend/domain/bookmarker/controller/BookmarkerControllerV1.java
@@ -0,0 +1,21 @@
+package com.mybrary.backend.domain.bookmarker.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Bookmarker 컨트롤러", description = "Bookmarker Controller API")
+@RestController
+@RequestMapping("/api/v1/bookmarker")
+public class BookmarkerControllerV1 {
+
+    /*
+    * API 명세서에 bookmark controller 관련 api 추가 필요
+    * */
+
+}
--- backend/src/main/java/com/mybrary/backend/domain/mybrary/controller/MybraryControllerV1.java
+++ backend/src/main/java/com/mybrary/backend/domain/mybrary/controller/MybraryControllerV1.java
@@ -1,5 +1,7 @@
 package com.mybrary.backend.domain.mybrary.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,29 +11,34 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Mybrary 컨트롤러", description = "Mybrary Controller API")
 @RestController
 @RequestMapping("/api/v1/mybrary")
-public class MybraryController {
+public class MybraryControllerV1 {
 
+    @Operation(summary = "나의 마이브러리 조회", description = "나의 마이브러리 정보 조회")
     @GetMapping
     public ResponseEntity<?> getMybrary() {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "타인의 마이브러리 조회", description = "회원 아이디를 통한 회원의 마이브러리 정보 조회")
     @GetMapping("/{memberid}")
-    public ResponseEntity<?> getOtherMybrary(
+    public ResponseEntity<?> getMybraryById(
         @PathVariable(name = "memberid") long memberId) {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "마이브러리 생성", description = "마이브러리 생성")
     @PostMapping
     public ResponseEntity<?> createMybrary() {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "마이브러리 수정", description = "마이브러리 정보 수정")
     @PutMapping
     public ResponseEntity<?> updateMybrary() {
 
