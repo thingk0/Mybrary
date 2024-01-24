4807f2130bcbc28f4517877281ec32913c7b9ff4
박헤선 <qkr8364@naver.com>
2024-01-24 14:31:39 +0900

    FEAT: MemberDto 등등 작성

---
+++ backend/src/main/java/com/mybrary/backend/domain/member/dto/MyFollowingDto.java
@@ -0,0 +1,21 @@
+package com.mybrary.backend.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MyFollowingDto {
+
+    private String memberId;
+    private String name;
+    private String nickname;
+    private String profileImageUrl;
+
+}
--- backend/src/main/java/com/mybrary/backend/domain/member/dto/SignupRequestDto.java
+++ backend/src/main/java/com/mybrary/backend/domain/member/dto/SignupRequestDto.java
@@ -18,18 +18,19 @@ public class SignupRequestDto {
     @NotEmpty(message = "이메일은 비워둘 수 없습니다.")
     private String email;
 
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$])(?=.*\\d).{3,20}$",
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$])(?=.*\\d).{3,20}$",
              message = "비밀번호는 영문 대소문자, 숫자, 특수문자(!, @, #, $)를 조합하여 3~20자 이내여야 합니다.")
     private String password;
 
     @NotEmpty(message = "비밀번호 확인은 비워둘 수 없습니다.")
     private String passwordConfirm;
 
-    @NotEmpty(message = "이름을 입력해야 합니다.")
+    @Pattern(regexp = "^([가-힇]){3,20}$",
+             message = "이름은 한글(자음 또는 모음만 존재하는 것 제외)을 조합하여 3~20자 이내여야 합니다.")
     private String name;
 
-    @Pattern(regexp = "^[a-zA-Z0-9_]{2,20}$",
-             message = "닉네임은 영문자, 숫자 및 언더바(_)를 포함할 수 있으며 2~20자 이내여야 합니다.")
+    @Pattern(regexp = "^[a-zA-z0-9]{4,12}$",
+             message = "닉네임은 영문자, 숫자 및 언더바(_)를 포함할 수 있으며 4~20자 이내여야 합니다.")
     private String nickname;
 
 }
