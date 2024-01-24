dd4de7cd611c80aefd42adda4d7adadab6a2e89a
고명성 <audtjd9080@naver.com>
2024-01-24 09:01:15 +0900

    REFACTOR: API 문서화 및 정리
    - 기존 PaperController 클래스에 Swagger 문서화 어노테이션 추가. 각 API 엔드포인트에 `@Operation` 어노테이션 적용하여 API 설명 제공.
    - 클래스 명을 PaperController 에서 PaperControllerV1로 변경. 버전 관리 및 API 업데이트 시 효율적인 관리를 위한 조치.

--- backend/src/main/java/com/mybrary/backend/domain/contents/paper/controller/PaperControllerV1.java
+++ backend/src/main/java/com/mybrary/backend/domain/contents/paper/controller/PaperControllerV1.java
@@ -1,10 +1,9 @@
 package com.mybrary.backend.domain.contents.paper.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Paper 컨트롤러", description = "Paper Controller API")
 @RequestMapping("/api/v1/paper")
 @RestController
-public class PaperController {
+public class PaperControllerV1{
 
+    @Operation(summary = "페이퍼 스크랩", description = "페이퍼 스크랩")
     @GetMapping("/{paperid}/scrap")
     public ResponseEntity<?> scrapPaper(
         @PathVariable(name = "paperid") String paperId) {
@@ -24,13 +24,14 @@ public class PaperController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "페이퍼 공유", description = "페이퍼 다른 사용자에게 공유")
     @PostMapping("/share")
     public ResponseEntity<?> sharePaper(@RequestBody String test) {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
+    @Operation(summary = "페이퍼 댓글 조회", description = "페이퍼 아이디를 통한 페이퍼의 댓글 조회")
     @GetMapping("/{paperid}/comments")
     public ResponseEntity<?> getPaperComments(
         @PathVariable(name = "paperid") String paperId) {
