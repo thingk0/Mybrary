fa815926206a83870fa8ccdae720285b6eba6e16
박헤선 <qkr8364@naver.com>
2024-01-26 08:55:24 +0900

    FIX: 수정

--- backend/src/main/java/com/mybrary/backend/domain/category/controller/CategoryControllerV1.java
+++ backend/src/main/java/com/mybrary/backend/domain/category/controller/CategoryControllerV1.java
@@ -4,9 +4,15 @@ import com.mybrary.backend.domain.book.dto.BookGetDto;
 import com.mybrary.backend.domain.category.dto.CategoryGetDto;
 import com.mybrary.backend.domain.category.dto.CategoryPostDto;
 import com.mybrary.backend.domain.category.dto.CategoryUpdateDto;
+<<<<<<< Updated upstream
 import com.mybrary.backend.domain.category.service.CategoryService;
+=======
+import com.mybrary.backend.domain.category.service.CategoryServiceImpl;
+import com.mybrary.backend.domain.member.dto.MemberInfoDto;
+>>>>>>> Stashed changes
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,13 +40,30 @@ public class CategoryControllerV1 {
     public ResponseEntity<?> getCategoriesById(
         @RequestParam(name = "bookshelf-id") Long bookshelfId) {
 
-        return new ResponseEntity<List<CategoryGetDto>>(HttpStatus.OK);
+        CategoryGetDto category1 = new CategoryGetDto(1L, "여행", 1, 5);
+        CategoryGetDto category2 = new CategoryGetDto(2L, "공부", 3, 3);
+        CategoryGetDto category3 = new CategoryGetDto(3L, "일상", 2, 17);
+        CategoryGetDto category4 = new CategoryGetDto(4L, "만화", 4, 20);
+
+        List<CategoryGetDto> list = new ArrayList<>();
+        list.add(category1);
+        list.add(category2);
+        list.add(category3);
+        list.add(category4);
+
+        return new ResponseEntity<List<CategoryGetDto>>(list, HttpStatus.OK);
     }
 
     //    이건 Book 도메인 내에 들어가야 하는게 더 어룰리는것 같긴 함
     @Operation(summary = "카테고리 책 리스트 조회", description = "카테고리 아이디를 통한 책 목록 조회")
     @GetMapping("/category/{categoryId}")
     public ResponseEntity<?> getBooksByCategoryId(@PathVariable Long categoryId) {
+
+//        MemberInfoDto member1 = new MemberInfoDto()
+//
+//
+//        BookGetDto book1 = new BookGetDto(1L, )
+//        List<BookGetDto> list = new ArrayList<>();
 
         return new ResponseEntity<List<BookGetDto>>(HttpStatus.OK);
     }
