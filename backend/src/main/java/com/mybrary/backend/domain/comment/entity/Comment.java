5f384d9dd920f65bf2b17444355b2e50f01d2246
고명성 <audtjd9080@naver.com>
2024-01-26 00:12:32 +0900

    REFINE: 댓글 엔티티의 지연 로딩 전략 적용 및 매핑 최적화
    - 댓글 엔티티의 `FetchType.LAZY` 적용으로 성능 향상.
    - `Paper`, `Member`, `parentComment` 필드에 지연 로딩 적용하여 불필요한 즉시 로딩 방지.
    - `childComments`의 매핑을 `mappedBy`를 사용하여 명시적으로 설정.
    - 성능 최적화와 유지 보수의 용이성을 위한 리팩토링 진행.

--- backend/src/main/java/com/mybrary/backend/domain/comment/entity/Comment.java
+++ backend/src/main/java/com/mybrary/backend/domain/comment/entity/Comment.java
@@ -4,6 +4,7 @@ import com.mybrary.backend.domain.contents.paper.entity.Paper;
 import com.mybrary.backend.domain.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,23 +30,19 @@ public class Comment {
     @Column(name = "comment_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "paper_id")
     private Paper paper;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_comment_id")
     private Comment parentComment;
 
-    /*
-    * 양방향 관계 설정
-    * */
-
-    @OneToMany
+    @OneToMany(mappedBy = "parentComment")
     private List<Comment> childComments = new ArrayList<Comment>();
 
     private String content;
