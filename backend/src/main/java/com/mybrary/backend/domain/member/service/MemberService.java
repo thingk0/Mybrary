297a0d3ac27fd7ddd840625aee71e4861b3aac9d
고명성 <audtjd9080@naver.com>
2024-01-26 23:09:19 +0900

    FEAT: 회원가입 로직 구현
    - PasswordEncoder, MemberRepository 을 주입받도록 구성함.
    - create 메소드 구현:
      - 회원가입 시 비밀번호 불일치를 검사하고, 불일치 시 PasswordMismatchException 을 발생시킴.
      - 이메일 중복 검사를 수행하고, 중복된 이메일이 있을 경우 DuplicateEmailException 을 발생시킴.
      - 비밀번호를 암호화하여 저장하고, SignupRequestDto 에서 Member 엔티티로 변환하여 저장함.
      - 생성된 Member ID 값을 반환함.
    - 중복 이메일 사용을 방지하여 시스템의 안정성을 제공함.

--- backend/src/main/java/com/mybrary/backend/domain/member/service/MemberService.java
+++ backend/src/main/java/com/mybrary/backend/domain/member/service/MemberService.java
@@ -1,4 +1,7 @@
 package com.mybrary.backend.domain.member.service;
 
+import com.mybrary.backend.domain.member.dto.SignupRequestDto;
+
 public interface MemberService {
+    Long create(SignupRequestDto requestDto);
 }
