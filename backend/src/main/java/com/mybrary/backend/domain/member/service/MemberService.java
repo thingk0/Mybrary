04b635c9d8732aee7c3d6e9590295f28acfd7f44
고명성 <audtjd9080@naver.com>
2024-01-29 15:50:21 +0900

    FEAT: 로그인 로직 추가 및 JWT 토큰 관리 구현
    - 로그인 로직을 구현:
      - login 메소드 추가하여 로그인 요청을 처리. 이메일이 존재하지 않거나 비밀번호가 일치하지 않는 경우에 적절한 예외를 발생시킴.
      - JWT 토큰을 생성하고, RefreshTokenRepository 를 사용하여 새로운 RefreshToken 을 저장.
      - Access-Token 을 쿠키에 추가하여 클라이언트에 전달.
    - JWT 관련 클래스와 기능을 추가:
      - JwtProvider, CookieUtil, RefreshTokenRepository 를 주입받아 사용.
      - JWT 토큰 생성, 검증 및 쿠키 관리 기능을 구현하여 보안성 강화 및 인증/인가 과정 개선.
    - create 메소드 수정:
      - Member 엔티티 생성 로직에서 비밀번호 암호화 부분을 Member.of 팩토리 메소드 내로 이동. 이는 엔티티 생성 로직을 정리함.

--- backend/src/main/java/com/mybrary/backend/domain/member/service/MemberService.java
+++ backend/src/main/java/com/mybrary/backend/domain/member/service/MemberService.java
@@ -1,7 +1,12 @@
 package com.mybrary.backend.domain.member.service;
 
+import com.mybrary.backend.domain.member.dto.LoginRequestDto;
 import com.mybrary.backend.domain.member.dto.SignupRequestDto;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface MemberService {
+
     Long create(SignupRequestDto requestDto);
+
+    void login(LoginRequestDto requestDto, HttpServletResponse httpServletResponse);
 }
--- backend/src/main/java/com/mybrary/backend/domain/member/service/impl/MemberServiceImpl.java
+++ backend/src/main/java/com/mybrary/backend/domain/member/service/impl/MemberServiceImpl.java
@@ -1,24 +1,37 @@
 package com.mybrary.backend.domain.member.service.impl;
 
+import com.mybrary.backend.domain.member.dto.LoginRequestDto;
 import com.mybrary.backend.domain.member.dto.SignupRequestDto;
 import com.mybrary.backend.domain.member.entity.Member;
 import com.mybrary.backend.domain.member.repository.MemberRepository;
 import com.mybrary.backend.domain.member.service.MemberService;
-import com.mybrary.backend.global.exception.DuplicateEmailException;
-import com.mybrary.backend.global.exception.PasswordMismatchException;
+import com.mybrary.backend.global.cookie.CookieUtil;
+import com.mybrary.backend.global.exception.member.DuplicateEmailException;
+import com.mybrary.backend.global.exception.member.InvalidLoginAttemptException;
+import com.mybrary.backend.global.exception.member.PasswordMismatchException;
 import com.mybrary.backend.global.format.ErrorCode;
+import com.mybrary.backend.global.jwt.JwtProvider;
+import com.mybrary.backend.global.jwt.dto.TokenInfo;
+import com.mybrary.backend.global.jwt.repository.RefreshTokenRepository;
+import com.mybrary.backend.global.jwt.token.RefreshToken;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
+    private final JwtProvider jwtProvider;
+    private final CookieUtil cookieUtil;
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
+    @Transactional
     public Long create(SignupRequestDto requestDto) {
 
         /* 비밀번호 불일치 */
@@ -32,12 +45,41 @@ public class MemberServiceImpl implements MemberService {
                             throw new DuplicateEmailException(ErrorCode.MEMBER_EMAIL_DUPLICATED);
                         });
 
-        /* 비밀번호 암호화  */
-        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
-
-        Member member = Member.from(requestDto);
+        Member member = Member.of(requestDto, passwordEncoder.encode(requestDto.getPassword()));
         memberRepository.save(member);
         return member.getId();
+    }
+
+    @Override
+    @Transactional
+    public void login(LoginRequestDto requestDto, HttpServletResponse response) {
+
+        Member member = memberRepository.findByEmail(requestDto.getEmail())
+                                        .orElseThrow(
+                                            () -> new InvalidLoginAttemptException(
+                                                ErrorCode.MEMBER_LOGIN_FAILED));
+
+        if (!validatePassword(requestDto.getPassword(), member.getPassword())) {
+            throw new PasswordMismatchException(ErrorCode.MEMBER_LOGIN_FAILED);
+        }
+
+        refreshTokenRepository.findByValue(member.getEmail())
+                              .ifPresent(refreshTokenRepository::delete);
+
+        TokenInfo tokenInfo = jwtProvider.generateTokenInfo(member.getEmail());
+        refreshTokenRepository.save(RefreshToken.builder()
+                                                .key(tokenInfo.getRefreshToken())
+                                                .value(tokenInfo.getEmail())
+                                                .accessToken(tokenInfo.getAccessToken())
+                                                .build());
+
+        cookieUtil.addCookie("Access-Token", tokenInfo.getAccessToken(),
+                             jwtProvider.getACCESS_TOKEN_TIME() + 60,
+                             response);
+    }
+
+    private boolean validatePassword(String input, String encoded) {
+        return passwordEncoder.matches(input, encoded);
     }
 
 }
