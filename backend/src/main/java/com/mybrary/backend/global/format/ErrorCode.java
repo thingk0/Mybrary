8676d3379a0fb150223260a702964cd8c4a072f2
고명성 <audtjd9080@naver.com>
2024-01-29 14:59:21 +0900

    FEAT: 세부 예외 상황에 따른 응답 코드 추가
    - 요청 처리 중 발생할 수 있는 예외에 대한 에러 코드 추가
     - HTTP 메서드, 미디어 타입과 관련된 예외 등
    - 인증 및 인가 예외에 대한 에러 코드 추가
     - 토큰 없음, 토큰 만료, 잘못된 토큰 형식 등
    - 회원 가입, 로그인과 관련한 예외 처리를 위한 에러 코드 추가
     - 이메일 중복, 존재하지 않는 이메일, 로그인 실패 등등

--- backend/src/main/java/com/mybrary/backend/global/format/ErrorCode.java
+++ backend/src/main/java/com/mybrary/backend/global/format/ErrorCode.java
@@ -11,12 +11,34 @@ public enum ErrorCode {
     // 글로벌 예외 처리
     GLOBAL_UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "처리 중 예기치 않은 서버 오류가 발생했습니다."),
 
+    // 요청 관련 예외 처리
+    REQUEST_METHOD_NOT_SUPPORTED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드입니다."),
+    MEDIA_TYPE_NOT_SUPPORTED(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 미디어 타입입니다."),
+    MISSING_SERVLET_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "필수 요청 파라미터가 누락되었습니다."),
+    SERVLET_REQUEST_BINDING_EXCEPTION(HttpStatus.BAD_REQUEST, "요청 바인딩 오류가 발생했습니다."),
+    CONVERSION_NOT_SUPPORTED(HttpStatus.INTERNAL_SERVER_ERROR, "지원되지 않는 변환 유형입니다."),
+    TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "타입 불일치 오류가 발생했습니다."),
+    MESSAGE_NOT_READABLE(HttpStatus.BAD_REQUEST, "메시지를 읽을 수 없습니다."),
+    MESSAGE_NOT_WRITABLE(HttpStatus.INTERNAL_SERVER_ERROR, "메시지를 쓸 수 없습니다."),
+    METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "메서드 인자가 유효하지 않습니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "인증에 실패했습니다."),
+
+    ACCESS_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "액세스 토큰을 찾을 수 없습니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "리프레쉬 토큰을 찾을 수 없습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다."),
+    INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "로그인 정보 형식이 올바르지 않습니다."),
+    MODIFIED_TOKEN_DETECTED(HttpStatus.UNAUTHORIZED, "로그인 정보가 변경되었습니다."),
+    INVALID_TOKEN_FORMAT(HttpStatus.UNAUTHORIZED, "토큰 형식이 유효하지 않습니다."),
+    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "서비스 이용을 위해 로그인이 필요합니다."),
+
     // 회원 관련 예외 처리
     MEMBER_SIGNUP_FAILED(HttpStatus.BAD_REQUEST, "회원 가입에 실패했습니다."),
     MEMBER_LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인 정보가 정확하지 않습니다."),
-    MEMBER_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "입력하신 비밀번호가 일치하지 않습니다."),
-    MEMBER_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다.");
-
+    MEMBER_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    MEMBER_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
+    MEMBER_EMAIL_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 이메일입니다."),
+    ;
 
     private final HttpStatus status;
     private final String message;
