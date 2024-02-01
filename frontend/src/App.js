ff84b8fff48126bff5788a6db6ed47076043ac13
서만기 <seo_m98@naver.com>
2024-02-01 21:36:16 +0900

    FIX: 웹소켓 관련 주석처리
    Change-Id: I692d0005dd63e54df4315ae0260cf5ba338e6a8b

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,19 @@ import { Outlet } from "react-router-dom";
 import Nav from "./components/atom/Nav";
 import "./App.css";
 
-import { useEffect } from "react";
-import useStompStore from "./store/useStompStore";
-import { checkLogin } from "./api/member/CheckLogin";
-import { useNavigate } from "react-router-dom";
-import useUserStore from "./store/useUserStore";
-import useNotificationStore from "./store/useNotificationStore";
+// import { useEffect } from "react";
+// import useStompStore from "./store/useStompStore";
+// import { checkLogin } from "./api/member/CheckLogin";
+// import { useNavigate } from "react-router-dom";
+// import useUserStore from "./store/useUserStore";
+// import useNotificationStore from "./store/useNotificationStore";
 import { Toaster } from "react-hot-toast";
 
 export default function App() {
-  const { connect, disconnect } = useStompStore();
-  const { setNewNotification } = useNotificationStore();
-  const navigate = useNavigate();
-  const email = useUserStore((state) => state.user.email);
+  // const { connect, disconnect } = useStompStore();
+  // const { setNewNotification } = useNotificationStore();
+  // const navigate = useNavigate();
+  // const email = useUserStore((state) => state.user.email);
 
   // useEffect(() => {
   //   // 로그인 중인지 확인하는 요청 보내고, 로그인 상태가 아니라면 return
