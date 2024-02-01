40f4c38e1202696d3c2eebe1628203c981172563
박헤선 <qkr8364@naver.com>
2024-02-01 16:10:47 +0900

    FEAT: Notification 서비스 레포지토리 추가
    Change-Id: Ibb53c4c2133d3ec02c8489fc42f7f6fe4ee8106c

--- backend/src/main/java/com/mybrary/backend/domain/notification/service/NotificationService.java
+++ backend/src/main/java/com/mybrary/backend/domain/notification/service/NotificationService.java
@@ -1,4 +1,17 @@
 package com.mybrary.backend.domain.notification.service;
 
+import com.mybrary.backend.domain.notification.dto.NotificationGetDto;
+import com.mybrary.backend.domain.notification.dto.NotificationPostDto;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
 public interface NotificationService {
+
+    List<NotificationGetDto> getAllNotification(Long myId, Pageable page);
+
+    void deleteNotification(Long notifyId);
+
+    void deleteAllNotification(Long myId);
+
+    void saveNotification(NotificationPostDto notification);
 }
