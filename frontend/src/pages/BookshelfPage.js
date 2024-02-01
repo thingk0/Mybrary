2c77e171651f5049ddfaecb32d71c854a5c689d9
서만기 <seo_m98@naver.com>
2024-02-01 21:39:28 +0900

    REFACTOR: 책장페이지에서 꺼내간 컴포넌트를 정리.
    Change-Id: I754a9a807c4851dfc16ab91be2bbfac5ecadb740

--- frontend/src/pages/BookshelfPage.js
+++ frontend/src/pages/BookshelfPage.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
-import Container from "../components/frame/Container";
 import styles from "./style/BookshelfPage.module.css";
-import title from "../components/atom/atomstyle/Title.module.css";
-import s from "classnames";
+import Container from "../components/frame/Container";
 import CategoryEditModal from "../components/bookshelf/CategoryEditModal";
-import { useNavigate } from "react-router-dom";
+import BookshelfHeader from "../components/bookshelf/BookshelfHeader";
+import Bookshelf from "../components/bookshelf/Bookshelf";
+// import { useNavigate } from "react-router-dom";
 
 export default function BookshelfPage() {
-  const navigate = useNavigate();
   const [categoryList, setCategoryList] = useState([]);
-  // 책장 ID 상태 (필요한 경우에 사용)
-  // const [bookShelfId, setBookShelfId] = useState(null);
 
   useEffect(() => {
     const fetchedData = {
@@ -68,102 +65,10 @@ export default function BookshelfPage() {
     setCategoryList(sortedCategoryList);
   }, []);
 
-  //책들 랜더링 하는 부분
-  function BookAlbum({ category }) {
-    const colors = [
-      "#946087",
-      "#BA667E",
-      "#F5998F",
-      "#455371",
-      "#57423F",
-      "#7E994F",
-      "#2F4858",
-    ];
-
-    const bookCount = category ? category.bookCount : 0;
-    return (
-      <>
-        {Array.from({
-          length: bookCount < 10 ? bookCount : 10,
-        }).map((_, idx) => {
-          const height = Math.random() * 50 + 90;
-          const color = colors[Math.floor(Math.random() * colors.length)];
-          return (
-            <div
-              key={idx}
-              className={styles.album}
-              style={{
-                height: `${height}px`,
-                backgroundColor: color,
-              }}
-            ></div>
-          );
-        })}
-        {bookCount - 10 > 0 && <div>+ {bookCount - 10}</div>}
-      </>
-    );
-  }
-
-  //책장을 랜더링하는 부분
-  function Bookshelf({ category, categoryName, sort }) {
-    return (
-      <div
-        className={styles.boxs}
-        style={{
-          flex: `${
-            category && category.categoryId !== "empty"
-              ? category.bookCount > 10
-                ? 10
-                : category.bookCount < 4
-                ? 4
-                : category.bookCount
-              : 2
-          }`,
-        }}
-      >
-        <div className={styles.box}>
-          {category && category.categoryId !== "empty" && (
-            <>
-              <div className={s(categoryName, styles.categoryName)}>
-                {category.categoryName}
-              </div>
-              <div className={s(sort, styles.books)}>
-                <div className={styles.categoryNameOnHover}>
-                  {category.bookCount}권의 "{category.categoryName}"책 보러가기
-                </div>
-                <BookAlbum category={category} />
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
       <Container>
-        <div>
-          <div className={title.back} onClick={() => navigate("../")}>
-            &lt; 뒤로가기
-          </div>
-        </div>
-        <div className={title.title}>
-          <div
-            className={title.left_title}
-            onClick={() => navigate("../threads")}
-          >
-            &lt; 게시물
-          </div>
-          <div className={title.main_title}>cwnsgh's bookshelf</div>
-          <div
-            className={title.right_title}
-            onClick={() => navigate("../rollingpaper")}
-          >
-            {" "}
-            롤링페이퍼 &gt;
-          </div>
-        </div>
+        <BookshelfHeader />
         <div>
           <div className={styles.middle}>
             <div>책갈피목록</div>
