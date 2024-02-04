8c47463cfd4e1a813eb9e5bf9d8c8cfbb2ff3375
고명성 <audtjd9080@naver.com>
2024-02-04 17:52:16 +0900

    REFACTOR: JWT 필터 응답 처리 로직 모듈화 및 클래스 이름 변경
    - FilterResponse 클래스 생성을 통해 필터 단에서 발생하는 예외 상황에 대한 JSON 응답을 모듈화. 이를 통해 다양한 예외 처리 시 재사용 가능한 응답 로직 구현으로 코드의 중복을 최소화하고, 관리의 효율성 향상.
    - TokenExceptionFilterHandler 클래스의 이름을 TokenExceptionFilter 로 변경하고, 새롭게 생성한 FilterResponse 클래스를 활용하여 JWT 검증 과정에서 발생하는 예외에 대해 표준화된 응답 방식 적용.
    
    Change-Id: Iaca5d3a8c901b5b1002ea64be0e149db66f38f98

--- backend/src/main/java/com/mybrary/backend/global/filter/TokenExceptionFilter.java
+++ backend/src/main/java/com/mybrary/backend/global/filter/TokenExceptionFilter.java
@@ -1,34 +1,32 @@
-package com.mybrary.backend.global.handler;
+package com.mybrary.backend.global.filter;
 
 import com.mybrary.backend.global.exception.jwt.UnauthorizedAccessException;
+import com.mybrary.backend.global.format.code.FilterResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Slf4j
+@Log4j2
 @Component
-public class TokenExceptionFilterHandler extends OncePerRequestFilter {
+@RequiredArgsConstructor
+public class TokenExceptionFilter extends OncePerRequestFilter {
+
+    private final FilterResponse filterResponse;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-
         try {
             filterChain.doFilter(request, response);
         } catch (UnauthorizedAccessException e) {
-            response.setStatus(401);
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            String body = "{\n"
-                + "              \"status\" : \"FAILED\",\n"
-                + "              \"message\" : \"로그인이 필요한 서비스입니다.\"\n"
-                + "         }";
-            response.getWriter().write(body);
+            log.error("TokenExceptionFilter : {} -> \nUnauthorizedAccessException : {}", request.getRequestURI(), e.getMessage(), e);
+            filterResponse.sendLoginRequiredResponse(response);
         }
     }
 }
