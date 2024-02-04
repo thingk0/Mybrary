d7aa5c75df9ea9b795ce0ce385369f9e683820e2
고명성 <audtjd9080@naver.com>
2024-02-04 19:29:03 +0900

    FEAT: Swagger 설정 업데이트 및 보안 스키마 추가
    - API 문서화를 위한 Swagger 설정 클래스에 보안 스키마를 추가하여, JWT 기반 인증 메커니즘을 문서에 통합.
    - `customOpenAPI` 메소드를 통해 `BearerAuth` 보안 스키마를 등록, 이를 통해 API 테스트 시 JWT 토큰을 손쉽게 적용 가능.
    
    Change-Id: Ia2fcbf75b7b0b6ea491929332d93b3acb21c48b2

--- backend/src/main/java/com/mybrary/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/mybrary/backend/global/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.mybrary.backend.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +17,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    // swagger 그룹 추가
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+            .components(new Components()
+                            .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT")));
+    }
 
+    // swagger 그룹 추가
     // 전체보기
     @Bean
     public GroupedOpenApi all() {
