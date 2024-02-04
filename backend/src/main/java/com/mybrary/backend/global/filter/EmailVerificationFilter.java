e0bab04ef82c207bf64bf0b0a6a8d13c528f185e
고명성 <audtjd9080@naver.com>
2024-02-04 19:07:33 +0900

    REFACTOR: 이메일 인증 필터 로직 개선 및 응답 메시지 구조화
    - `EmailVerificationFilter` 클래스에서 응답 메시지 생성 로직을 `FilterResponse` 클래스를 통해 구조화하여 관리하도록 변경
    - 로그 메시지에 더 상세한 정보를 추가하여 디버깅 시 이해도를 높임. 특히, 이메일 인증이 실패했을 때 요청 URI 를 로그에 포함시켜 문제 진단을 용이하게 함.
    - 필터 로직에서 `@RequiredArgsConstructor` 어노테이션을 사용하여 필요한 의존성을 final 로 선언하고, 생성자 주입 방식을 통해 안전하게 의존성을 주입받도록 구조 개선.
    
    Change-Id: I73d0f453fe56b6b4462875597b8228969c892411

--- backend/src/main/java/com/mybrary/backend/global/filter/EmailVerificationFilter.java
+++ backend/src/main/java/com/mybrary/backend/global/filter/EmailVerificationFilter.java
@@ -1,22 +1,25 @@
 package com.mybrary.backend.global.filter;
 
+import com.mybrary.backend.global.format.code.FilterResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Log4j2
 @Component
+@RequiredArgsConstructor
 public class EmailVerificationFilter extends OncePerRequestFilter {
 
-    private final String SIGNUP_URL = "/api/v1/member";
-    private final String POST_METHOD = "POST";
-
+    private final FilterResponse filterResponse;
+    final String SIGNUP_URL = "/api/v1/member";
+    final String POST_METHOD = "POST";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -29,8 +32,8 @@ public class EmailVerificationFilter extends OncePerRequestFilter {
 
         Cookie[] cookies = request.getCookies();
         if (cookies == null || !isConfirmedEmailCookiePresent(cookies)) {
-            log.warn("확인된 이메일 쿠키가 존재하지 않습니다. 회원가입을 위해서는 이메일 인증이 필요합니다.");
-            sendAuthFailureResponse(response);
+            log.warn("[이메일 인증 필터] 이메일 인증 << 쿠키 미발견 >>. 인증 실패 응답 전송: {}", request.getRequestURI());
+            filterResponse.sendEmailAuthFailureResponse(response);
             return;
         }
 
@@ -45,17 +48,6 @@ public class EmailVerificationFilter extends OncePerRequestFilter {
         }
         return false;
     }
-
-    private static void sendAuthFailureResponse(HttpServletResponse response) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        String body = "{\n"
-            + "  \"status\" : \"FAILED\",\n"
-            + "  \"message\" : \"회원가입을 위해서는 이메일 인증이 필요합니다.\"\n"
-            + "}";
-        response.getWriter().write(body);
-    }
-
 }
+
 
