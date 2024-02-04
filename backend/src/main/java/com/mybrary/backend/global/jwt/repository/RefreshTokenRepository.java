43fd148909a41519bfa71338ba975dc2a4194bbb
고명성 <audtjd9080@naver.com>
2024-02-04 17:45:44 +0900

    REFACTOR: 토큰 조회 메서드 수정.
    - RefreshTokenRepository 의 조회 메서드 명칭을 findByValue 에서 findByRefreshToken 으로 변경.
    
    Change-Id: Ie8c583d8d28056cd0743952a1650e9069c769703

--- backend/src/main/java/com/mybrary/backend/global/jwt/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/mybrary/backend/global/jwt/repository/RefreshTokenRepository.java
@@ -1,9 +1,9 @@
 package com.mybrary.backend.global.jwt.repository;
 
-import com.mybrary.backend.global.jwt.token.RefreshToken;
+import com.mybrary.backend.global.jwt.RefreshToken;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-    Optional<RefreshToken> findByValue(String email);
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken,String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
