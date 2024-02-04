21f79be78f0e706732d66c33550d291c4cdbfd11
고명성 <audtjd9080@naver.com>
2024-02-04 19:18:58 +0900

    REFACTOR: JwtProvider 클래스의 기능 개선 및 구조 변경
    - JWT 토큰 생성 및 검증 로직을 포함하는 `JwtProvider` 클래스에서 토큰 파싱 및 검증 로직을 개선. 특히, `Jwts.parserBuilder()`를 JWT 파싱 방식으로 변경함.
    - 또한, 클래스명을 `TokenProvider`로 변경.
    - `validateToken` 메소드에서 다양한 JWT 예외를 처리하여, 예외 상황에 대한 로깅을 강화.
    
    Change-Id: I0071199db12ce853035c8730a97a7f3638c30eef

--- backend/src/main/java/com/mybrary/backend/global/jwt/provider/TokenProvider.java
+++ backend/src/main/java/com/mybrary/backend/global/jwt/provider/TokenProvider.java
@@ -1,17 +1,15 @@
-package com.mybrary.backend.global.jwt;
+package com.mybrary.backend.global.jwt.provider;
 
-import com.mybrary.backend.global.exception.jwt.InvalidTokenFormatException;
-import com.mybrary.backend.global.exception.jwt.InvalidTokenSignatureException;
-import com.mybrary.backend.global.format.ErrorCode;
-import com.mybrary.backend.global.jwt.dto.TokenInfo;
+import com.mybrary.backend.global.jwt.TokenInfo;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
@@ -21,27 +19,27 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-
 @Log4j2
 @Component
-public class JwtProvider {
+public class TokenProvider {
 
     @Getter
-    @Value("${jwt.ACCESS_TIME}")
-    private int ACCESS_TOKEN_TIME;
+    private final int ACCESS_TOKEN_TIME;
 
     @Getter
-    @Value("${jwt.REFRESH_TIME}")
-    private int REFRESH_TOKEN_TIME;
+    private final int REFRESH_TOKEN_TIME;
 
-    @Value("${jwt.SECRET_KEY}")
-    private String SECRET_KEY;
+    private final String SECRET_KEY;
 
     private Key key;
 
-    @PostConstruct
-    public void init() {
-        key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
+    public TokenProvider(@Value("${jwt.ACCESS_TIME}") final int ACCESS_TOKEN_TIME,
+                         @Value("${jwt.REFRESH_TIME}") final int REFRESH_TOKEN_TIME,
+                         @Value("${jwt.SECRET_KEY}") final String SECRET_KEY) {
+        this.ACCESS_TOKEN_TIME = ACCESS_TOKEN_TIME;
+        this.REFRESH_TOKEN_TIME = REFRESH_TOKEN_TIME;
+        this.SECRET_KEY = SECRET_KEY;
+        this.key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(this.SECRET_KEY));
     }
 
     public TokenInfo generateTokenInfo(String email) {
@@ -71,31 +69,38 @@ public class JwtProvider {
 
     private Claims parseClaims(String token) {
         try {
-            Claims body = Jwts.parser()
-                              .setSigningKey(key)
-                              .parseClaimsJws(token)
-                              .getBody();
-            return body;
+            return Jwts.parserBuilder()
+                       .setSigningKey(key)
+                       .build()
+                       .parseClaimsJws(token)
+                       .getBody();
         } catch (ExpiredJwtException e) {
             log.info("만료된 토큰, 사용자 이메일: {}", e.getClaims().getSubject());
             return e.getClaims();
+        } catch (JwtException e) {
+            log.error("토큰 파싱 실패: {}", e.getMessage());
+            throw new RuntimeException("토큰 파싱 실패: {}", e);
         }
     }
 
-    protected boolean verifyToken(String token) {
+    public boolean validateToken(String token) {
         try {
-            Jwts.parser()
-                .setSigningKey(key)
-                .parseClaimsJws(token)
-                .getBody();
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
         } catch (ExpiredJwtException e) {
-            return false;
-        } catch (UnsupportedJwtException | MalformedJwtException e) {
-            throw new InvalidTokenFormatException(ErrorCode.INVALID_TOKEN_TYPE);
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (MalformedJwtException e) {
+            log.info("잘못된 형식의 JWT 토큰입니다.");
         } catch (SignatureException e) {
-            throw new InvalidTokenSignatureException(ErrorCode.MODIFIED_TOKEN_DETECTED);
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        } catch (JwtException e) {
+            log.info("JWT 토큰 처리 중 알 수 없는 예외가 발생했습니다.");
         }
-        return true;
+        return false;
     }
 
     public String extractEmail(String token) {
