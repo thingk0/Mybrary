0f38a3ef730bb25de3a2df3d8e1ed2ae04c275d0
고명성 <audtjd9080@naver.com>
2024-02-04 18:36:49 +0900

    CHORE: EnableConfigurationProperties 어노테이션 추가
    - `EnableConfigurationProperties` 어노테이션 추가: Spring Boot 구성 속성을 활성화하여 application.yml 파일에 정의된 설정을 더 쉽게 바인딩하고 관리할 수 있게 해, 구성 관리의 유연성과 코드의 간결성을 향상시키기 위함.
    
    Change-Id: Icc10a5e7ed8b32d4d6af0eb62e9f2a18b9aa2dc1

--- backend/src/main/java/com/mybrary/backend/BackendApplication.java
+++ backend/src/main/java/com/mybrary/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.mybrary.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties
 public class BackendApplication {
 
     public static void main(String[] args) {
