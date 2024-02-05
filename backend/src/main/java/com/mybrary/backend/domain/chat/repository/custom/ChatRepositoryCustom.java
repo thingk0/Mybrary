75122355ea49b0a5a421047a64c06ec0efbf4eae
여진구 <dmdkvj369@naver.com>
2024-02-05 16:07:16 +0900

    DELETE: 백엔드코드삭제

--- backend/src/main/java/com/mybrary/backend/domain/chat/repository/custom/ChatRepositoryCustom.java
+++
@@ -1,5 +0,0 @@
-package com.mybrary.backend.domain.chat.repository.custom;
-
-public interface ChatRepositoryCustom {
-
-}
--- backend/src/main/java/com/mybrary/backend/domain/member/repository/custom/QuerydslMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mybrary.backend.domain.member.repository.custom;
-
-import com.mybrary.backend.domain.member.entity.Member;
-import java.util.Optional;
-
-public interface QuerydslMemberRepository {
-    Optional<Member> findByEmail(String email);
-}
