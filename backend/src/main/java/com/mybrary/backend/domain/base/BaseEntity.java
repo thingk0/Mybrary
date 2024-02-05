51564e4f3790ff82a177bedf40d02d8d4f77bd4b
고명성 <audtjd9080@naver.com>
2024-02-05 13:28:50 +0900

    REFACTOR: BaseEntity 클래스의 JPA 컬럼 정의 명시화
    - created_at, modified_at, is_deleted 컬럼명으로 각 필드를 명시적으로 매핑, 데이터베이스와의 직관적인 연결성 강화.
    
    Change-Id: I709c4eaead52be6c7657db23f497f4496514e082

--- backend/src/main/java/com/mybrary/backend/domain/base/BaseEntity.java
+++ backend/src/main/java/com/mybrary/backend/domain/base/BaseEntity.java
@@ -5,12 +5,9 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.userdetails.User;
 
 @Getter
 @MappedSuperclass
@@ -18,15 +15,14 @@ import org.springframework.security.core.userdetails.User;
 public class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "modified_at")
     private LocalDateTime modifiedAt;
 
+    @Column(name = "is_deleted")
     private boolean isDeleted;
 
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-    }
 }
