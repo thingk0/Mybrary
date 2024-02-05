b04cced31b258121ae8ec970c04e15f5c01e8dfd
고명성 <audtjd9080@naver.com>
2024-02-05 10:41:20 +0900

    FEAT: 닉네임 유효성 검사 인터셉터 구현
    - NicknameValidInterceptor 추가.
    - /api/v1/member/nickname/** 경로에 대해 닉네임 유효성 검사 인터셉터 적용.
    
    Change-Id: I538668fff833b6474f22c196026b76ca5c26bb78

--- backend/src/main/java/com/mybrary/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/mybrary/backend/global/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.mybrary.backend.global.config;
 
+import com.mybrary.backend.global.interceptor.NicknameValidInterceptor;
 import com.mybrary.backend.global.resolver.AccessTokenArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,9 +14,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final AccessTokenArgumentResolver accessTokenArgumentResolver;
+    private final NicknameValidInterceptor nicknameValidInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(accessTokenArgumentResolver);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(nicknameValidInterceptor)
+                .addPathPatterns("/api/v1/member/nickname/**");
+    }
 }
+
