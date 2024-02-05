519ed3856a7fabd4fd9578935cd87c71f9f4f354
여진구 <dmdkvj369@naver.com>
2024-02-05 16:20:43 +0900

    DELETE: 백엔드 빠이

--- backend/src/main/java/com/mybrary/backend/global/jwt/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mybrary.backend.global.jwt.repository;
-
-import com.mybrary.backend.global.jwt.RefreshToken;
-import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken,String> {
-    Optional<RefreshToken> findByRefreshToken(String refreshToken);
-}
