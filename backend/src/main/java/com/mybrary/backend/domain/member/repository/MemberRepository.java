95d09127e3e72606d1be8f5434a733fbc4bff54f
고명성 <audtjd9080@naver.com>
2024-02-05 10:46:38 +0900

    REFACTOR: MemberRepository 상속 관계 변경
    - QuerydslMemberRepository 대신 MemberRepositoryCustom 을 상속받도록 MemberRepository 수정.
    
    Change-Id: I903e6350849c79a929d2e74758ab68966068b4ef

--- backend/src/main/java/com/mybrary/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/mybrary/backend/domain/member/repository/MemberRepository.java
@@ -2,13 +2,13 @@ package com.mybrary.backend.domain.member.repository;
 
 import com.mybrary.backend.domain.follow.entity.Follow;
 import com.mybrary.backend.domain.member.entity.Member;
-import com.mybrary.backend.domain.member.repository.custom.QuerydslMemberRepository;
+import com.mybrary.backend.domain.member.repository.custom.MemberRepositoryCustom;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslMemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     @Query("select m from Member m inner join Follow f on m.id = f.following.id where f.follower.id = :myId and f.isDeleted = false ")
     List<Member> getAllFollowing(@Param("myId") Long myId);
