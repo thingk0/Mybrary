82e3786c928b53e24c374ac9b5f6ca17216f2f42
고명성 <audtjd9080@naver.com>
2024-02-05 10:52:00 +0900

    CHORE: 불필요한 임포트 제거
    - 사용되지 않는 임포트 문(org.springframework.security.core.parameters.P) 삭제.
    
    Change-Id: If4540db7b117d6e69e7825732c306067e11e2b79

--- backend/src/main/java/com/mybrary/backend/domain/follow/repository/FollowRepository.java
+++ backend/src/main/java/com/mybrary/backend/domain/follow/repository/FollowRepository.java
@@ -1,13 +1,9 @@
 package com.mybrary.backend.domain.follow.repository;
 
 import com.mybrary.backend.domain.follow.entity.Follow;
-import com.mybrary.backend.domain.member.entity.Member;
-import com.mybrary.backend.domain.member.repository.custom.QuerydslMemberRepository;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
