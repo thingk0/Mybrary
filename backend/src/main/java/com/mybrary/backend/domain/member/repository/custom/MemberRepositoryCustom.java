6d2417cee9213cede2b4a9753d8eecf97cf33668
고명성 <audtjd9080@naver.com>
2024-02-05 10:46:03 +0900

    REFACTOR: 인터페이스 이름 변경 및 닉네임 중복 검사 메서드 추가
    - QuerydslMemberRepository 를 MemberRepositoryCustom 으로 이름 변경.
    - 닉네임 중복 검사 기능을 위한 isNicknameDuplicate 메서드 추가.
    
    Change-Id: I979342a6125d5afe9cd69d0dd921d7a61a752fe4

--- backend/src/main/java/com/mybrary/backend/domain/member/repository/custom/MemberRepositoryCustom.java
+++ backend/src/main/java/com/mybrary/backend/domain/member/repository/custom/MemberRepositoryCustom.java
@@ -3,6 +3,8 @@ package com.mybrary.backend.domain.member.repository.custom;
 import com.mybrary.backend.domain.member.entity.Member;
 import java.util.Optional;
 
-public interface QuerydslMemberRepository {
+public interface MemberRepositoryCustom {
     Optional<Member> findByEmail(String email);
+    boolean isNicknameDuplicate(String email);
+
 }
