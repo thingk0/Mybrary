da11d325b354a02cbec3ee144ec87154b65f65a2
최준호 <cojuna@naver.com>
2024-02-07 13:03:57 +0900

    FEAT : api baseurl추가
    Change-Id: I48013a56620c1f4cbf21f3ac1431ed6bfd47ccfb

--- frontend/src/api/member/Logout.js
+++ frontend/src/api/member/Logout.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+const BASE_URL = "http://thingk0.duckdns.org:8080/api/v1/";
 export async function doLogout() {
   try {
-    const response = await axios.post("/api/v1/member/logout");
+    const response = await axios.post(BASE_URL + "member/logout");
     localStorage.clear();
     return response.data;
   } catch (error) {
