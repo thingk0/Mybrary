622e6855a5dd1b4bf53602014191e8bcb3b45a67
고명성 <audtjd9080@naver.com>
2024-02-12 14:25:41 +0900

    REFACTOR: WebSocket 설정 변경 및 인터셉터 추가
    - WebSocketConfig 에 ChatStompInterceptor 를 추가하여, WebSocket 메시지 교환 시 인터셉터를 통한 추가적인 처리를 가능하게 함.
    - 메시지 브로커 및 애플리케이션 목적지 접두사 설정 추가로, WebSocket 메시지 라우팅 설정
    
    Change-Id: I67b99167b3b9ce0a26cf29e29d72d095adc3be5d

--- backend/src/main/java/com/mybrary/backend/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/mybrary/backend/global/config/WebSocketConfig.java
@@ -1,40 +1,38 @@
 package com.mybrary.backend.global.config;
 
-import org.springframework.context.annotation.Bean;
+import com.mybrary.backend.global.interceptor.ChatStompInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 
+@Log4j2
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final ChatStompInterceptor chatStompInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //stomp의 접속 주소
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").withSockJS();
     }
-
 
     @Override
-    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
-        registry.setMessageSizeLimit(2048 * 2048);
-        registry.setSendBufferSizeLimit(2048 * 2048);
-        registry.setSendTimeLimit(2048 * 2048);
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 
-
-    @Bean
-    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxTextMessageBufferSize(2048 * 2048);
-        container.setMaxSessionIdleTimeout(2048L * 2048L);
-        container.setAsyncSendTimeout(2048L * 2048L);
-        container.setMaxBinaryMessageBufferSize(2048 * 2048);
-        return container;
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(chatStompInterceptor);
     }
+
 }
