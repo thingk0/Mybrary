7c5fc694df06c2cb6e3a5fbe55ff67f80812da69
박헤선 <qkr8364@naver.com>
2024-02-13 03:39:19 +0900

    FEAT: 웹소켓 config 설정 추가
    Change-Id: I949cdaf7214db2c87f7cb5e5d562fffe03262326

--- backend/src/main/java/com/mybrary/backend/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/mybrary/backend/global/config/WebSocketConfig.java
@@ -3,12 +3,15 @@ package com.mybrary.backend.global.config;
 import com.mybrary.backend.global.interceptor.ChatStompInterceptor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 
 @Log4j2
@@ -35,6 +38,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(chatStompInterceptor);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.setMessageSizeLimit(2048 * 2048);
+        registry.setSendBufferSizeLimit(2048 * 2048);
+        registry.setSendTimeLimit(2048 * 2048);
+    }
+
+    // 해당 설정 필수! (톰캣 사용 시)
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(2048 * 2048);
+        container.setMaxSessionIdleTimeout(2048L * 2048L);
+        container.setAsyncSendTimeout(2048L * 2048L);
+        // container.setMaxBinaryMessageBufferSize(2048 * 2048);
+        return container;
     }
 
 }
