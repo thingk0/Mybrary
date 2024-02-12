d887617fda5c9d942221725c8cad2713dd0a15b0
고명성 <audtjd9080@naver.com>
2024-02-12 14:25:21 +0900

    FEAT: ChatRoomRepositoryCustom 인터페이스에 새로운 메서드 추가
    - fetchMyChatRoomList 메서드를 추가하여, 사용자 이메일 기반으로 채팅방 목록 조회 기능 구현.
    - chatRoomIdList2 메서드에 Pageable 지원 추가하여, 채팅방 목록 페이징 기능 강화.
    
    Change-Id: I7f83311da33d7612eb5368b8e5877c13186ed176

--- backend/src/main/java/com/mybrary/backend/domain/chat/repository/custom/ChatRoomRepositoryCustom.java
+++ backend/src/main/java/com/mybrary/backend/domain/chat/repository/custom/ChatRoomRepositoryCustom.java
@@ -1,10 +1,14 @@
 package com.mybrary.backend.domain.chat.repository.custom;
 
+import com.mybrary.backend.domain.chat.dto.responseDto.ChatRoomResponseDto;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ChatRoomRepositoryCustom {
 
     Optional<List<Long>> chatRoomIdList2(Long myId, Pageable page);
+
+    Page<ChatRoomResponseDto> fetchMyChatRoomList(String email, Pageable pageable);
 }
