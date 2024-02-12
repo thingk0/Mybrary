b31411c974e2fc2432b0d9904d52c1f5953d7e72
tiger3127 <tiger6453127@naver.com>
2024-02-12 16:11:29 +0900

    FIX: likeService 약간 수정

--- backend/src/main/java/com/mybrary/backend/domain/contents/like/service/LikeServiceImpl.java
+++ backend/src/main/java/com/mybrary/backend/domain/contents/like/service/LikeServiceImpl.java
@@ -12,7 +12,7 @@ public class LikeServiceImpl implements LikeService {
 
     /* 사용자가 해당 paper에 좋아요를 눌렀는지 여부 판단 */
     @Override
-    public boolean checkIsLiked(Long paperId, Long memberId) {
+    public boolean checkIsLiked(Long memberId, Long paperId) {
         return likeRepository.isLikedPaper(paperId, memberId).orElse(null) != null;
 
     }
