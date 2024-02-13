6fd0208209c2e874be3d11ffde5aa15f40f233b5
고명성 <audtjd9080@naver.com>
2024-02-13 14:13:11 +0900

    CONFIG: WebSocketConfig 에 인증 핸들러 로직 변경
    - WebSocketConfig 에서 인증 처리 방식을 개선함. DefaultHandshakeHandler 를 커스텀 핸들러로 교체하여, 액세스 토큰 추출 및 검증 과정을 내부에서 직접 처리함.
    - TokenService 와 TokenProvider 를 이용하여 액세스 토큰을 검증하고, 유효한 토큰의 경우에만 웹소켓 연결을 허용함.
    
    Change-Id: Ib3ce86158c2cb9afd2fb7b888a1cb5dbc120279d

--- backend/src/main/java/com/mybrary/backend/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/mybrary/backend/global/config/WebSocketConfig.java
@@ -1,19 +1,27 @@
 package com.mybrary.backend.global.config;
 
+import com.mybrary.backend.global.exception.jwt.AccessTokenNotFoundException;
 import com.mybrary.backend.global.handler.ChatErrorHandler;
 import com.mybrary.backend.global.interceptor.ChatStompInterceptor;
-import com.mybrary.backend.global.interceptor.WebSocketAuthenticationHandshakeInterceptor;
+import com.mybrary.backend.global.jwt.provider.TokenProvider;
+import com.mybrary.backend.global.jwt.service.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 
 @Log4j2
@@ -22,18 +30,38 @@ import org.springframework.web.socket.server.standard.ServletServerContainerFact
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final ChatErrorHandler chatErrorHandler;
-    private final ChatStompInterceptor chatStompInterceptor;
-    private final WebSocketAuthenticationHandshakeInterceptor authenticationHandshakeInterceptor;
+    private final TokenService tokenService;
+    private final TokenProvider tokenProvider;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
-                .setHandshakeHandler(authenticationHandshakeInterceptor)
+                .setHandshakeHandler(new DefaultHandshakeHandler() {
+                    @Override
+                    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler,
+                                                      Map<String, Object> attributes) {
+
+                        String accessToken = tokenService.extractAccessToken((HttpServletRequest) request);
+                        log.info("accessToken = {}", accessToken);
+
+                        if (accessToken == null) {
+                            throw new AccessTokenNotFoundException();
+                        }
+
+                        return new Principal() {
+                            private final String name = tokenProvider.extractEmail(accessToken);
+
+                            @Override
+                            public String getName() {
+                                return name;
+                            }
+                        };
+                    }
+                })
                 .withSockJS();
 
-        registry.setErrorHandler(chatErrorHandler);
+        registry.setErrorHandler(new ChatErrorHandler());
     }
 
     @Override
@@ -44,7 +72,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(chatStompInterceptor);
+        registration.interceptors(new ChatStompInterceptor());
     }
 
     @Override
