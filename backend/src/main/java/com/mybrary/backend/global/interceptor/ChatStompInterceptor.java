b9880f6424e8e470121e573dcdf8feba3b085b0c
고명성 <audtjd9080@naver.com>
2024-02-13 15:54:24 +0900

    REFACTOR: Hot FIX 2
    Change-Id: Ib1e38f0a484171b823061f5741f25c1dc2e2a506

--- backend/src/main/java/com/mybrary/backend/global/interceptor/ChatStompInterceptor.java
+++ backend/src/main/java/com/mybrary/backend/global/interceptor/ChatStompInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,7 @@ public class ChatStompInterceptor implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
 
         log.info("message = {}", message);
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
         if (StompCommand.CONNECT == accessor.getCommand()) {
             String jwtToken = accessor.getFirstNativeHeader("Authorization");
             log.info("CONNECT authorizationHeader={}", jwtToken);
