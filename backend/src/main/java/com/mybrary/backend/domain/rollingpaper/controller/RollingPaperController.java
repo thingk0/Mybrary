d3a9b9f508cd6e44bec3fd74b3bfae31c497655b
고명성 <audtjd9080@naver.com>
2024-02-15 00:13:35 +0900

    REFACTOR: 롤링페이퍼 컨트롤러 구조 변경
    - 롤링페이퍼 컨트롤러의 메서드 이름 변경:
      - 'getRollingPaper' 메서드를 'fetch' 로, 'saveRollingPaper' 메서드를 'update' 로 변경
    - 롤링페이퍼 저장 메서드의 HTTP 메서드 변경:
      - 롤링페이퍼를 저장하는 메서드의 HTTP 메서드를 POST 에서 PUT 으로 변경
    - 롤링페이퍼 컨트롤러에서 'SimpMessagingTemplate' 및 관련 메서드 'sendRollingPaper' 삭제:
      - 웹소켓 관련 코드를 이전
    
    Change-Id: Ibe7892fbf3469737b969863f25033615a870157f

--- backend/src/main/java/com/mybrary/backend/domain/rollingpaper/controller/RollingPaperController.java
+++ backend/src/main/java/com/mybrary/backend/domain/rollingpaper/controller/RollingPaperController.java
@@ -7,17 +7,13 @@ import com.mybrary.backend.global.format.response.ResponseCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,12 +27,11 @@ public class RollingPaperController {
 
     private final RollingPaperService rollingPaperService;
     private final ApiResponse response;
-    private final SimpMessagingTemplate template;
 
     @Operation(summary = "롤링페이퍼 조회", description = "롤링페이퍼 입장")
     @GetMapping("/{id}")
-    public ResponseEntity<?> getRollingPaper(@Parameter(hidden = true) Authentication authentication,
-                                             @PathVariable("id") Long rollingPaperId) {
+    public ResponseEntity<?> fetch(@Parameter(hidden = true) Authentication authentication,
+                                   @PathVariable("id") Long rollingPaperId) {
 
         return response.success(ResponseCode.ROLLING_PAPER_FETCHED,
                                 rollingPaperService.fetch(authentication.getName(), rollingPaperId));
@@ -44,23 +39,12 @@ public class RollingPaperController {
     }
 
     @Operation(summary = "롤링페이퍼 저장", description = "롤링페이퍼 저장")
-    @PostMapping
-    public ResponseEntity<?> saveRollingPaper(@Parameter(hidden = true) Authentication authentication,
-                                              @RequestBody RollingPaperDto rollingPaper) {
+    @PutMapping
+    public ResponseEntity<?> update(@Parameter(hidden = true) Authentication authentication,
+                                    @RequestBody RollingPaperDto rollingPaper) {
 
         return response.success(ResponseCode.ROLLING_PAPER_SAVED,
                                 rollingPaperService.update(authentication.getName(), rollingPaper));
-    }
-
-    @MessageMapping("/rollingPaper/{rollingPaperId}")
-    public void sendRollingPaper(@DestinationVariable Long rollingPaperId,
-                                 Principal principal,
-                                 RollingPaperDto rollingPaper) {
-
-        log.info("method=sendRollingPaper rollingPaperId={}, email={}, rollingPaperString={}",
-                 rollingPaperId, principal.getName(), rollingPaper.getRollingPaperString());
-
-        template.convertAndSend("/sub/rollingPaper/" + rollingPaperId, rollingPaper);
     }
 
 }
